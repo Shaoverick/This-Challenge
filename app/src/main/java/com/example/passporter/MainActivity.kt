b31981c2de6038de462467fbbdf0855c99e72de2
Miguel <shaoverick@gmail.com>
2022-02-24 22:49:59 +0100

    Empty project creation

---
+++ app/src/main/java/com/example/passporter/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.passporter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.passporter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    //region PROPERTIES ----------------------------------------------------------------------------
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    //endregion
+
+
+    //region LIFECYCLE -----------------------------------------------------------------------------
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+    //endregion
+
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        return navController.navigateUp(appBarConfiguration)
+//                || super.onSupportNavigateUp()
+//    }
+
+}
