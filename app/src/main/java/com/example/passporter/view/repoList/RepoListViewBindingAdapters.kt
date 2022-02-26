625c62282cc47378a90d05cbbd256626ea3221fb
Miguel <shaoverick@gmail.com>
2022-02-26 19:55:32 +0100

    Show owner avatar image in card, by Glide

--- app/src/main/java/com/example/passporter/view/repoList/RepoListViewBindingAdapters.kt
+++ app/src/main/java/com/example/passporter/view/repoList/RepoListViewBindingAdapters.kt
@@ -1,20 +1,24 @@
 package com.example.passporter.view.repoList
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.domain.model.entity.RepoEntity
+import com.example.domain.model.entity.UrlString
 import com.example.passporter.view.repoList.adapter.RepoRVAdapter
-
-@BindingAdapter("setAdapter")
-fun setAdapter(recyclerView: RecyclerView, adapter: RepoRVAdapter?) {
-    adapter?.let {
-        recyclerView.adapter = it
-    }
-}
+import com.google.android.material.imageview.ShapeableImageView
 
 @BindingAdapter("addRepoList")
 fun addRepoList(recyclerView: RecyclerView?, repoList: MutableList<RepoEntity>?) {
     val adapter = recyclerView?.adapter
     if (adapter is RepoRVAdapter && repoList != null)
         adapter.addData(repoList)
+}
+
+@BindingAdapter("setImage")
+fun setImage(imageView: ImageView, urlString: UrlString) {
+    Glide.with(imageView.context)
+        .load(urlString)
+        .into(imageView)
 }
