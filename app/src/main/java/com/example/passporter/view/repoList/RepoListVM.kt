2338b9af8a8f2d1768c557bc5aaec2a0c4348a5b
Miguel <shaoverick@gmail.com>
2022-02-26 14:43:55 +0100

    set recycler view to show GitHub repository list. Added pagination

--- app/src/main/java/com/example/passporter/view/repoList/RepoListVM.kt
+++ app/src/main/java/com/example/passporter/view/repoList/RepoListVM.kt
@@ -1,14 +1,13 @@
-package com.example.passporter.view.repositoryList
+package com.example.passporter.view.repoList
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.domain.model.entity.OwnerEntity
 import com.example.domain.model.entity.RepoEntity
 import com.example.domain.useCase.gitHubRepo.GetUserRepoListParams
 import com.example.domain.useCase.gitHubRepo.GetUserRepoListUC
-import com.example.passporter.Event
-import com.example.passporter.UIResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -27,15 +26,24 @@ class RepoListVM @Inject constructor(
 
 
     //region PROPERTIES ----------------------------------------------------------------------------
-    private val _repoList = MutableLiveData<Event<UIResult<List<RepoEntity>>>>()
-    val repoList: LiveData<Event<UIResult<List<RepoEntity>>>> = _repoList
+    private val _repoList = MutableLiveData<List<RepoEntity>>()
+    val repoList: LiveData<List<RepoEntity>> = _repoList
+
+    private var page = 0
     //endregion
 
 
+    init {
+        getRepoListPage()
+    }
+
+
     //region PUBLIC METHODS ------------------------------------------------------------------------
-    fun getRepoList(page: Int) {
+    fun getRepoListPage() {
+        page++
+
         viewModelScope.launch {
-            val result = withContext(Dispatchers.IO) {
+            val result: List<RepoEntity> = withContext(Dispatchers.IO) {
                 getUserRepoListUC.execute(
                     GetUserRepoListParams(
                         userName = USER_NAME,
@@ -43,11 +51,26 @@ class RepoListVM @Inject constructor(
                         page = page
                     )
                 )
-            }
+            }.peekSuccessOrNull() ?: arrayListOf()
 
-            _repoList.postValue(Event(UIResult.fromResult(result)))
+            _repoList.postValue(result)
+//            _repoList.postValue(mock())
         }
     }
     //endregion
+
+
+    //For fast mocking purposes
+    private fun mock(): List<RepoEntity> {
+        return listOf(RepoEntity(
+            name = "nombre del repo",
+            description = "descripción del repo",
+            ownerEntity = OwnerEntity(
+                login = "Míguel",
+                avatarUrl = null
+            ),
+            fork = true
+        ))
+    }
 
 }
